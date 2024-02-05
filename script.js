b877bf77d1f834d9decbf10b1f7e794999bde06c
James Steed <james.steed23@student.cart.org>
2024-02-05 10:35:53 -0800

    added all of the inputs and direction for JS

---
+++ script.js
@@ -0,0 +1,8 @@
+function runner (nameInput, presetInput, certifyInput, signedInput) {
+
+//Constructs object from parameters 
+let objVar = {name : nameInput, preset: presetInput, certification: certifyInput, signer: signedInput}
+
+
+
+}
