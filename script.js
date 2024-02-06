ab24885de03712c452cfc9ddf1479e40f71a9a41
James Steed <james.steed23@student.cart.org>
2024-02-06 10:40:26 -0800

    added all images into respective folders & did JS

--- script.js
+++ script.js
@@ -1,8 +1,107 @@
-function runner (nameInput, presetInput, certifyInput, signedInput) {
+const objCertIn = {
+    tiktok: {
+        title: 'TikTok Shop \"Entrepreneur\"',
+        desc: 'Thinks that selling basically useless things on the TikTok shop is going to get them money and fame.'
+    },
 
-//Constructs object from parameters 
-let objVar = {name : nameInput, preset: presetInput, certification: certifyInput, signer: signedInput}
+    brainrot: {
+        title: 'Professional Brain Rot Spreader',
+        desc: 'Watched so much brain rot on social media \'Ironically\' that they have become brain rot themselves.'
+    },
+
+    him: {
+        title: 'Being literally him',
+        desc: 'Consumed so much of their favorite niche that they actually think they are that person from their niche.'
+    },
+
+    foryou: {
+        title: 'For-You page resposter',
+        desc: 'Resposts everything on their For-You page so much that everyone\'s For-You page is just theirs'
+    },
+
+    quagsire: {
+        title: 'Shiny Quagsire Handler',
+        desc: 'Searched a millenia for a shiny Wooper to evolve into a Quagsire. When it finally happened, their life was complete.'
+    },
+
+    sigma: {
+        title: 'A Sigma',
+        desc: 'Followed sigma rules for 5 years. Once complete, this award is mailed by Patrick Bateman himself.'
+    },
+
+    loner: {
+        title: 'A Loner',
+        desc: '\"I don\'t need anyone!\" says this person. Always stayed away from functions and never socialized outside of work/school. ;-;'
+    }
+}
+
+const objPresetIn = {
+    curtain: {
+        // Would have a method that changes the background to curtain & seal
+    },
+
+    obscure: {
+        // Would have a method that changes the background to obscure fast food place & seal
+    },
+
+    basement: {
+        // Would have a method that changes the background to a basement & seal
+    }
+}
 
 
+function runner(objCertify, objPreset) {
+
+    // Constructs object thru parameters.
+    this.name = nameInput;
+    this.preset = presetInput;
+    this.certification = certifyInput;
+    this.signer = signedInput;
+
+    //Changes background based on input
+    switch (objPreset.preset) {
+        case "curtain":
+            // Change background to curtains
+            break;
+
+        case "obscure":
+            // Change background to obscure fast food
+            break;
+
+        case "basement":
+            // Change background to basement
+            break;
+    }
+
+    //Changes certification based on input
+    switch (objCertify.certified) {
+        case "tiktok":
+            //Changes certification to tiktok
+            break;
+
+        case "brainrot":
+            //Changes certification to brainrot
+            break;
+
+        case "him":
+            //Changes certification to being him
+            break;
+
+        case "foryou":
+            //Changes certification to foryou page reposter
+            break;
+
+        case "quagsire":
+            //Changes certification to shiny quagsire
+            break;
+
+        case "sigma":
+            //Changes certification to being a sigma
+            break;
+
+        case "loner":
+            //Changes certification to being a loner
+            break;
+    }
 
 }
